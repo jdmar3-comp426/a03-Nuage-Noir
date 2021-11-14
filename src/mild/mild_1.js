8ee53a8ad703890aa7558e13ad8245ff9c11c7cb
Nuage-Noir <nuage@live.unc.edu>
2021-11-14 12:43:52 -0500

    maxmin

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -40,17 +40,9 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-    let max = numbers[0];
-    let min = numbers[0];
-    for (let i = 1; i<numbers.length; i++) {
-        if (numbers[i]>max) {
-            max = numbers[i];
-        }
-        else if (numbers[i]<min) {
-            min = numbers[i];
-        }
-    }
-    return {min, max};
+    let max = Math.max(...numbers);
+    let min = Math.min(...numbers);
+    return {max, min};
 }
 
 /**
