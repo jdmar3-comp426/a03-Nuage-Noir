9e38e2aba336b069c1a274eb214176f2384af100
Nuage-Noir <nuage@live.unc.edu>
2021-12-03 23:32:23 -0500

    spicy9 EvenOdd

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -180,7 +180,10 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    function test(x) {
+        return x % 2 === 0;
+    }
+    return everyEven(arr, test);
 };
 
 
@@ -190,7 +193,10 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    function test(x) {
+        return x % 2 === 0;
+    }
+    return someEven(arr, test);
 };
 
 
