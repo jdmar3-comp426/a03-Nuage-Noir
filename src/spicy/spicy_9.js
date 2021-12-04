163cbc03f5a7fa5f78e8a337b0f6dd8cac860623
Nuage-Noir <nuage@live.unc.edu>
2021-12-03 22:45:10 -0500

    spicy9 10*50

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -53,7 +53,7 @@ export const multiplyBy = (num1) => {
  * Use the multiplyBy function to create and export a function named
  *   "tenTimes" that multiplies a number by 10.
  */
-export const tenTimes = undefined;
+export const tenTimes = multiplyBy(10);
 
 
 /**
@@ -61,7 +61,7 @@ export const tenTimes = undefined;
  *   function to multiply 50 by 10 and returns the result.
  */
 export const tenTimesFifty = () => {
-
+    return tenTimes(50);
 };
 
 
